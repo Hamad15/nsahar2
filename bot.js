6abd5b5253f24125e4389708f69dd83af7cd514a
hamads1814 <47487348+hamads1814@users.noreply.github.com>
2019-02-10 02:08:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ console.log("BOT هههه.");
   return channel.send(`**السلام عليكم
 -أحادثينا حقيقية وتواجدنا دائم حتى وأن ضاقت بك الأوقات
 ‏دائم التواجد لذلك أقترب ولا تزيد أضعاف المسافة**. ♥
-https://discord.gg/hd566fy
+https://discord.gg/c42zQWN
 [ ${member}  ]
 @everyone
 @here
@@ -22,7 +22,7 @@ client.on("guildMemberAdd", member => {
   return channel.send(`**السلام عليكم
 -أحادثينا حقيقية وتواجدنا دائم حتى وأن ضاقت بك الأوقات
 ‏دائم التواجد لذلك أقترب ولا تزيد أضعاف المسافة**. ♥
-https://discord.gg/hd566fy
+https://discord.gg/c42zQWN
 [ ${member}  ]
 @everyone
 @here
